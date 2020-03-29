cee169e1896ec4cecd71b69c3010a9c1e1c0a1ce
nicmalegre <nikoalegre@hotmail.com>
2020-03-28 22:33:46 -0300

    HICE LAS MODIFICACIONES AL NIVEL SECUNDARIO. FUNCIONA TODO CON LOS BOTONES. ARCHIVOS ACTUALIZADOS.

---
+++ js/FuncionesCopia.js
@@ -0,0 +1,99 @@
+ //FUNCIONES PARA MOSTRAR Y ESCONDER ELEMENTOS EN LA PAGINA DE NIVELES
+  
+
+ function desplegarProg(){
+    if ((document.getElementById("programas").style.visibility) == "none")
+    {document.getElementById("programas").style.display = "block";
+    document.getElementById("text-programas").innerHTML = "Programas -";
+    }
+    else
+    {document.getElementById("programas").style.display = "none";
+    document.getElementById("text-programas").innerHTML = "Programas +";
+    //document.getElementById("div-primero").style.backgroundColor = "#1E3EB8";
+    }
+    return;
+}
+
+  function desplegarPrimer(){
+  if ((document.getElementById("archivos-primer").style.display) == "none")
+  {document.getElementById("archivos-primer").style.display = "block";
+  document.getElementById("text-Primer").innerHTML = "Primer Año -";
+  //document.getElementById("div-primero").style.backgroundColor = "rgb(11, 23, 68)";
+  }
+  else
+  {document.getElementById("archivos-primer").style.display = "none";
+  document.getElementById("text-Primer").innerHTML = "Primer Año +";
+  //document.getElementById("div-primero").style.backgroundColor = "#1E3EB8";
+  }
+  return;
+  }
+  
+  function desplegarSegundo(){
+  if ((document.getElementById("archivos-segundo").style.display) == "none")
+  {document.getElementById("archivos-segundo").style.display = "block";
+  document.getElementById("text-segundo").innerHTML = "Segundo Año -";
+  //document.getElementById("div-segundo").style.backgroundColor = "rgb(11, 23, 68)";
+  }
+  else
+  {document.getElementById("archivos-segundo").style.display = "none";
+  document.getElementById("text-segundo").innerHTML = "Segundo Año +";
+  //document.getElementById("div-segundo").style.backgroundColor = "#1E3EB8";
+  }
+  return;
+  }
+
+  function desplegarTercero(){
+    if ((document.getElementById("archivos-tercero").style.display) == "none")
+    {document.getElementById("archivos-tercero").style.display = "block";
+    document.getElementById("text-tercero").innerHTML = "Tercer Año -";
+    //document.getElementById("div-tercero").style.backgroundColor = "rgb(11, 23, 68)";
+    }
+    else
+    {document.getElementById("archivos-tercero").style.display = "none";
+    document.getElementById("text-tercero").innerHTML = "Tercer Año +";
+    //document.getElementById("div-tercero").style.backgroundColor = "#1E3EB8";
+    }
+    return;
+  }
+
+  function desplegarCuarto(){
+    if ((document.getElementById("archivos-cuarto").style.display) == "none")
+    {document.getElementById("archivos-cuarto").style.display = "block";
+    document.getElementById("text-cuarto").innerHTML = "Cuarto Año -";
+    //document.getElementById("div-cuarto").style.backgroundColor = "rgb(11, 23, 68)";
+    }
+    else
+    {document.getElementById("archivos-cuarto").style.display = "none";
+    document.getElementById("text-cuarto").innerHTML = "Cuarto Año +";
+    //document.getElementById("div-cuarto").style.backgroundColor = "#1E3EB8";
+    }
+    return;
+    }
+
+  function desplegarQuinto(){
+      if ((document.getElementById("archivos-quinto").style.display) == "none")
+      {document.getElementById("archivos-quinto").style.display = "block";
+      document.getElementById("text-quinto").innerHTML = "Quinto Año -";
+      //document.getElementById("div-quinto").style.backgroundColor = "rgb(11, 23, 68)";
+      }
+      else
+      {document.getElementById("archivos-quinto").style.display = "none";
+      document.getElementById("text-quinto").innerHTML = "Quinto Año +";
+      //document.getElementById("div-quinto").style.backgroundColor = "#1E3EB8";
+      }
+      return;
+  }
+
+  function desplegarSexto(){
+    if ((document.getElementById("archivos-sexto").style.display) == "none")
+    {document.getElementById("archivos-sexto").style.display = "block";
+    document.getElementById("text-sexto").innerHTML = "Sexto Año -";
+    //document.getElementById("div-sexto").style.backgroundColor = "rgb(11, 23, 68)";
+    }
+    else
+    {document.getElementById("archivos-sexto").style.display = "none";
+    document.getElementById("text-sexto").innerHTML = "Sexto Año +";
+    //document.getElementById("div-sexto").style.backgroundColor = "#1E3EB8";
+    }
+    return;
+  }
